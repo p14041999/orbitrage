b072c934bf712d2a1d8a7e6698c375c527d3f90e
p14041999 <p14041999@gmail.com>
2021-07-15 04:58:24 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import logo from "../assets/Vector.svg";
 import arrow from "../assets/arrow.svg";
-import navbtn from "../assets/navbtn.svg";
+import navbtn from "../assets/navbtn_new.svg";
 import "../styles/header.scss";
 
 class header extends Component {
   render() {
     return (
       <div className="header">
-        <button className="shadow navbtn">
+        <button className="shadow navbtn"><a href="http://t.me/orbitedge">
           <img src={navbtn} />
-        </button>
+        </a></button>
         <div>
           <img src={logo} />
         </div>
